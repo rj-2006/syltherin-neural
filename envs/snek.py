19423624c1407b85ac763a8b0d3bdc5033a792d6
rj-2006 <keturahu295@gmail.com>
2025-10-20 23:14:34 +0530

    python downgraded to 3.11 venv

--- envs/snek.py
+++ envs/snek.py
@@ -33,6 +33,8 @@ class snekEnv:
             self.snake = self.head
             self.food = None
             self.place_food()
+            return np.array([0])
+            """ place holder for now it'll return the game state in an array """
 
         def play_step(self, action):
             """
@@ -41,19 +43,39 @@ class snekEnv:
             checks if food was eaten, updates the score
             returns values score, reward and game over (boolean value true or false)
             """
-            pass
+            self.frame += 1
+            reward = 0
+            gameover = False
 
-        def is_collision(self):
+            self.move(action)
+            self.snake.inser(0, self.head)
+
+            if self.is_collision():
+                gameover = True
+                reward -= 10
+                return reward,gameover, self.score
+
+        def is_collision(pt):
             """
             checkss if snake has collided with itself or with the boundaries
             """
-            pass
+            if pt.x < 0 or pt.x >= self.width or pt.y < 0 or pt.y >= self.height:
+                return True
+            if pt.x in self.snake[1:]:
+                return True
+            return False
 
         def place_food(self):
             """
             places food on the screen
             if food is on snake body moves it elsewhere
             """
+            food_x = random.randint(0, (self.width // BLOCK_SIZE)) * BLOCK_SIZE
+            food_y = random.randint(0, (self.height// BLOCK_SIZE)) * BLOCK_SIZE
+            self.food = Point(food_x, food_y)
+            if self.food in self.snake:
+                self.place_food()
+
         def render():
             """
             renders the game window using pygame
